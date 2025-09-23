29b3ea06cddeed9e569aff9cfb68ec2a66b5014a
LSGamez <aluan014@gmail.com>
2025-09-23 18:08:46 -0300

    Teste

---
+++ Jogo Marajo Vozes Silenciadas/Assets/Script/Mov.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Mov : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float vel;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        float movH = Input.GetAxis("Horizontal");
+        float movV = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(movH, movV);
+    }
+}
