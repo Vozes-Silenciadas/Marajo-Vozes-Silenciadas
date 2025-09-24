2b9507dab5f3d9d25d666cf903cec4ea46396114
LSGamez <aluan014@gmail.com>
2025-09-24 18:57:33 -0300

    Criação de Movimento e Animação (Basicas)

--- Jogo Marajo Vozes Silenciadas/Assets/Script/Mov.cs
+++ Jogo Marajo Vozes Silenciadas/Assets/Script/Mov.cs
@@ -3,18 +3,45 @@ using UnityEngine;
 public class Mov : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float vel;
+
+    [SerializeField]
+    private float vel;
+    private Animator animator;
+    private SpriteRenderer sprite;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
 
     void Update()
     {
-        float movH = Input.GetAxis("Horizontal");
-        float movV = Input.GetAxis("Vertical");
+        float movH = Input.GetAxisRaw("Horizontal");
+        float movV = Input.GetAxisRaw("Vertical");
 
-        rb.linearVelocity = new Vector2(movH, movV);
+        rb.linearVelocity = new Vector2(movH, movV).normalized * vel;
+
+        if (Input.GetKey(KeyCode.LeftShift)) vel = 2;
+        else if (Input.GetKey(KeyCode.LeftControl)) vel = 0.5f;
+        else vel = 1;
+
+        if (movH != 0 || movV != 0)
+        {
+            if (movH < 0) sprite.flipX = true;
+            else if (movH > 0) sprite.flipX = false;
+
+            animator.SetBool("estaAndando", true);
+            animator.SetFloat("InputX", movH);
+            animator.SetFloat("InputY", movV);            
+        }
+        else
+        {
+            animator.SetBool("estaAndando", false);
+            animator.SetFloat("UltimoInputX", movH);
+            animator.SetFloat("UltimoInputY", movV);
+        }  
+       
     }
 }
