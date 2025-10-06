032c1e502ee35d8b0d760b1deacf34ed54e0ec16
Lor1606 <202424470021@ifba.edu.br>
2025-10-06 18:21:46 -0300

    Rotação de inimigo
    Fiz uma modificação no código de movimentação dos inimigos que faz que eles virem de acordo com a direção que estão andando

--- Jogo Marajo Vozes Silenciadas/Assets/Script/Inimigo_Mov.cs
+++ Jogo Marajo Vozes Silenciadas/Assets/Script/Inimigo_Mov.cs
@@ -1,9 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 public class Inimigo_Mov : MonoBehaviour
 {
     float dirX;
     float dirY;
+    float dirZ;
     Rigidbody2D rb;
     Vector3 localScale;
 
@@ -14,7 +16,7 @@ public class Inimigo_Mov : MonoBehaviour
         dirX = 1f;
       
     }
-
+    
 
 
     void Update()
@@ -24,21 +26,25 @@ public class Inimigo_Mov : MonoBehaviour
         {
             dirX = 0f;
             dirY = 1f;
+            transform.rotation = Quaternion.Euler(dirX, dirY, 180);
         }
         if (transform.position.y > 1.5f)
         {
             dirY = 0f;
             dirX = -1f;
+            transform.rotation = Quaternion.Euler(dirX, dirY, -90);
         }
         if (transform.position.x < 0f)
         {
             dirX = 0f;
             dirY = -1f;
+            transform.rotation = Quaternion.Euler(dirX, dirY, 0);
         }
         if (transform.position.y < 0f && transform.position.x < 3 )
         {
             dirX = 1f;
             dirY = 0f;
+            transform.rotation = Quaternion.Euler(dirX, dirY, 90);
         }
 
     }
