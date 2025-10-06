1ba4beb5facb3403b7552925b5d4ba9e0dbb5e91
LSGamez <aluan014@gmail.com>
2025-10-06 17:48:21 -0300

    Hotbar Criada e Coleta de itens modificada
    Crianção de imagens provisorias

--- Jogo Marajo Vozes Silenciadas/Assets/Script/Coleta_itens.cs
+++ Jogo Marajo Vozes Silenciadas/Assets/Script/Coleta_itens.cs
@@ -4,51 +4,78 @@ using UnityEngine;
 public class Coleta_itens : MonoBehaviour
 {
 
-    public TextMeshProUGUI texto;    
+    public TextMeshProUGUI texto;
+    inventarioControla inventarioControla;
+    public int contaPista;
+    void Start()
+    {
+        inventarioControla = FindAnyObjectByType<inventarioControla>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.CompareTag("Item"))
+        {            
+            Item item = collision.GetComponent<Item>();
+
+            if (item != null)
+            {
+                bool itemAdicionado = inventarioControla.AddItem(collision.gameObject);
+
+                if (itemAdicionado)
+                {
+                   // texto.text += $"Você tem {item.nome}\n";
+                    Destroy(collision.gameObject);
+                }
+            }            
+        }
+
+        if (collision.gameObject.CompareTag("Pistas"))
+        {
+            Item item = collision.GetComponent<Item>();
+            //texto.text += $"Você tem {item.nome}\n";
+            Destroy(collision.gameObject);
+
+            contaPista++;
+            texto.text = $"{contaPista}";
+        }
+        
         if (collision.gameObject.CompareTag("Ursinho"))
         {
-            Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem urso de pelucia!";
             Debug.Log("Conseguiu um urso de pelucia!");
-        }
-        if (collision.gameObject.CompareTag("Pato"))
-        {
             Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem pato de borracha!";
-            Debug.Log("Conseguiu um pato de borracha!");
         }
-        if (collision.gameObject.CompareTag("Bola"))
-        {
-            Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem uma bola de futebol!";
-            Debug.Log("Conseguiu uma bola de futebol!");
+            if (collision.gameObject.CompareTag("Pato"))
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("Conseguiu um pato de borracha!");
+            }
+            if (collision.gameObject.CompareTag("Bola"))
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("Conseguiu uma bola de futebol!");
+            }
+            if (collision.gameObject.CompareTag("Panela"))
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("Conseguiu uma panela de brinquedo!");
+            }
+            if (collision.gameObject.CompareTag("Boneca"))
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("Conseguiu a boneca de pano da Niara!");
+            }
+            if (collision.gameObject.CompareTag("Chave"))
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("Conseguiu uma chave!");
+            }
+            if (collision.gameObject.CompareTag("Cabra"))
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("Conseguiu um p� de cabra!");
+            }
         }
-        if (collision.gameObject.CompareTag("Panela"))
-        {
-            Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem uma panela de brinquedo!";
-            Debug.Log("Conseguiu uma panela de brinquedo!");
-        }
-        if (collision.gameObject.CompareTag("Boneca"))
-        {
-            Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem a boneca de pano da Niara!";
-            Debug.Log("Conseguiu a boneca de pano da Niara!");
-        }
-        if (collision.gameObject.CompareTag("Chave"))
-        {
-            Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem uma chave!";
-            Debug.Log("Conseguiu uma chave!");
-        }
-        if (collision.gameObject.CompareTag("Cabra"))
-        {
-            Destroy(collision.gameObject);
-            texto.text += $"\nvocê tem um pé de cabra";
-            Debug.Log("Conseguiu um p� de cabra!");
-        }
-    }
+
+
 }
---
+++ Jogo Marajo Vozes Silenciadas/Assets/Script/inventarioControla.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class inventarioControla : MonoBehaviour
+{
+
+    public GameObject hotbarPanel;
+    public GameObject slotPrefab;
+    public int quantiSlot;
+    public GameObject[] itemPrefab;
+
+    void Start()
+    {
+        for (int i = 0; i < quantiSlot; i++)
+        {
+            Slot slot = Instantiate(slotPrefab, hotbarPanel.transform).GetComponent<Slot>();
+
+            if (i < itemPrefab.Length)
+            {
+                GameObject item = Instantiate(itemPrefab[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.itemAtual = item;
+            }
+        }
+    }
+
+    public bool AddItem(GameObject itemPrefab)
+    {
+        foreach (Transform slotTransform in hotbarPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot != null && slot.itemAtual == null)
+            {
+                GameObject novoItem = Instantiate(itemPrefab, slotTransform);
+                novoItem.transform.localScale = slotTransform.localScale;
+                novoItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.itemAtual = novoItem;
+                return true;
+            }
+        }
+        return false;
+    }
+}
